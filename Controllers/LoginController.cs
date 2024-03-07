4318000f5250f395bab865d3a0d960854daca11e
Ard Nerus <mr.ard.nerus@gmail.com>
2024-03-07 08:55:41 +0545

    created BackEnd for Login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+
+        // POST: LoginController/Post
+        [HttpPost]
+        public ActionResult<string> PostLogin()
+        {
+            // Read the request body as a string
+            using var reader = new StreamReader(Request.Body, Encoding.UTF8);
+            var body = reader.ReadToEndAsync().Result;
+
+            // Parse the JSON string to a dynamic object
+            var jsonDocument = JsonDocument.Parse(body);
+            var root = jsonDocument.RootElement;
+
+            var credential = new LoginAPI.Model.Login_Data
+            {
+                Username = root.GetProperty("username").GetString(),
+                Password = root.GetProperty("password").GetString()
+            };
+
+            bool login_Status = LoginAPI.Factory.Login_Service_Handeler.Login_Valiator(credential);
+            if (!login_Status)
+            {
+                return Unauthorized("Invalid Username or Password");
+            }
+            return Ok("Login Successful");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://127.0.0.1:8001") // Adjust this to match your frontend application's origin
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowSpecificOrigin"); // Use the policy you defined
+
+app.UseRouting();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
